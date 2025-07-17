1e899c0e4128cfb0b524dc0d92269dbd077924c6
Hari <reddy9396@gmail.com>
2025-07-17 15:26:03 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source              = "git::https://github.com/Hari9396/Vnet-ChildModule.git//modules/vnet"
+  source              = "git::https://github.com/Hari9396/Vnet-ChildModule.git"
   resource_group_name = var.resource_group_name
   location            = var.location
   vnet_name           = var.vnet_name
